7d762c24fb4accc3a2f75df38130d03a2611d7a3
SK SAHIL UDDIN <sksahilu735@gmail.com>
2025-11-07 15:12:36 +0530

    updates

--- pyadmin/test.py
+++ pyadmin/test.py
@@ -1,22 +1,6 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import asyncio
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MONGO_URI = os.getenv("TRAIN_URL")
-
-async def test_connection():
-    try:
-        if not MONGO_URI:
-            print("❌ TRAIN_URL not found in .env file.")
-            return
-        client = AsyncIOMotorClient(MONGO_URI)
-        db = client["train-list"]
-        await db.command("ping")
-        print("✅ MongoDB Atlas connection successful.")
-    except Exception as e:
-        print("❌ MongoDB connection failed:", str(e))
-
-asyncio.run(test_connection())
+def test(x):
+    out = 1
+    for i in range(1, x):
+        out*=i
+    out*=x
+    return out
